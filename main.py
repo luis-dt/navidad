a28f93131199138d8cd1bcec90bc746725032692
luis-dt <danieltisoy2001@gmail.com>
2020-12-25 07:52:52 -0500

    feliz navidad

---
+++ main.py
@@ -0,0 +1,12 @@
+# Primer bucle, range(start,stop,step)
+for i in range(1, 30, 2):
+    if i == 1:
+        print(('*'*i).center(30))
+    else:
+        print(('^'*i).center(30))
+
+for leg in range(3):
+    print(('|||').center(30))
+print(('\\_____/').center(30))
+print(' '+30*'-')
+print('(¨¨¨)¡Feliz Navidad! (¨¨¨)')
